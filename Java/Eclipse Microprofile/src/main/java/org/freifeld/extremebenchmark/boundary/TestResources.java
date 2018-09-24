43b1173141742e536462a2eb35eff73fbfce517a
Royi Freifeld <freifeld.royi@gmail.com>
2018-09-24 21:28:18 +0300

    This thing

--- Java/Eclipse Microprofile/src/main/java/org/freifeld/extremebenchmark/boundary/TestResources.java
+++ Java/Eclipse Microprofile/src/main/java/org/freifeld/extremebenchmark/boundary/TestResources.java
@@ -1,13 +1,17 @@
 package org.freifeld.extremebenchmark.boundary;
 
+import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.Invocation;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+@RequestScoped
 @Path("/tests")
 public class TestResources {
 
@@ -16,12 +20,19 @@ public class TestResources {
 
     @GET
     @Path("/test-1-simple")
-    public String fetchSite() {
+    @Produces(MediaType.APPLICATION_JSON)
+    public String simpleString() {
+        return "Hello World!";
+    }
+
+    @GET
+    @Path("/test-2-site-fetch")
+    @Produces(MediaType.TEXT_HTML)
+    public Response fetchSite() {
         WebTarget target = this.httpClient.target("https://www.google.com");
         Invocation.Builder request = target.request();
         Response response = request.get();
-
-        return response.getEntity().toString();
+        return response;
     }
 
 }
